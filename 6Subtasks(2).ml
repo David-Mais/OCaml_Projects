b27912c038607052140b64e9ca8229f7f0d71c04
davidmaisuradze <maisuradze.davit@kiu.edu.ge>
2023-05-16 17:45:58 +0400

    added comments to second exercise

--- 6Subtasks(2).ml
+++ 6Subtasks(2).ml
@@ -1,9 +1,34 @@
+(*
+  ! Rewrite the following functions in a tail-recursive form:*)
+(*
+  ? let rec map f = function
+  ? | [] -> []
+  ? | x :: xs -> f x :: map f xs
+*)
+(*
+  ? let rec replicate n x =
+  ?   if n < 1 then [] else x :: replicate (n-1) x
+*)
+
+(*
+  * we define map function in tail recursive way. we recieve function and a list but in order
+  * to write function tail recursively we need accumulator so we define helper function inside
+  * map_tr that has accumulator and adds the affected elements of initial list into accumulator
+  * as soon as initial list is empty the helper returns accumulator and so does the map_tr
+*)
 let map_tr f l = 
   let rec helper f l acc = match l with
     |[] -> acc
     |hd::tl -> helper f tl (acc @ [f hd])
   in helper f l [];;
 
+(*
+  * we define replicate function in a tail recursive way. we recieve a number of times element
+  * should be replicated and an element of generic type meaning it can replicate integers strings...
+  * then we patterns match n so that whenever it reaches 0 we should return accumulator but if it matches
+  * with anyting else we call helper function with (n-1) the x (element we should replicate) and accumulator
+  * with element x applied
+*)
 let replicate_tr n x = 
   if (n < 0) then [] else
   let rec helper n x acc = match n with
